2d73be38011822ba83bc0ba09ae5293d4f597995
ecsunder15 <esunderland@renewfinancial.com>
2017-11-18 14:05:52 -0800

    added enemy spike, testing rotation script

---
+++ ExistenceIsPain/Assets/EnemyController.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyController : MonoBehaviour {
+
+	//how far away and how fast we rotate
+	public float rotateSpeed;
+	public float radius;
+
+	//point and angle at which we move around
+	private Vector2 target;
+	private float angle;
+
+	// Use this for initialization
+	public virtual void Start () {
+		target = transform.position;
+	}
+	
+	// Update is called once per frame
+	public virtual void Update () {
+		angle += rotateSpeed * Time.deltaTime;
+
+		//set the circular angle point thing to rotate around
+		var offset = new Vector2(Mathf.Sin(_angle), Mathf.Cos(_angle)) * Radius;
+		transform.position = _centre + offset;
+	}
+
+}
