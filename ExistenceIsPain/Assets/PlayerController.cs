f6e64ddcfad95659edfcf8971417bdb122f8f2d1
huesteus <huesteus@gmail.com>
2017-11-18 17:31:01 -0800

    Added audio

--- ExistenceIsPain/Assets/PlayerController.cs
+++ ExistenceIsPain/Assets/PlayerController.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 
+
 public class PlayerController : MonoBehaviour {
 
 	private int jumps = 0;
@@ -33,6 +34,8 @@ public class PlayerController : MonoBehaviour {
 
 	public bool dead = false;
 
+	public AudioClip MusicClip;
+	public AudioSource MusicSource;
 
 	// Use this for initialization
 	void Start () {
@@ -40,6 +43,8 @@ public class PlayerController : MonoBehaviour {
 
 		anim = GetComponent<Animator>();
 		facingLeft = false;
+
+		MusicSource.clip = MusicClip;
 	}
 	
 	// Update is called once per frame
@@ -75,6 +80,8 @@ public class PlayerController : MonoBehaviour {
 		if (jumps < maxJumps && Input.GetKeyDown (jump)) {
 			velocity.y += jumpSpeed;
 			jumps += 1;
+
+			MusicSource.Play ();
 
 			//dealDamage();
 
