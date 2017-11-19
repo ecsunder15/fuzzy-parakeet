8c93f8ed2674449527ecc12b7583b2e61fccb092
huesteus <huesteus@gmail.com>
2017-11-19 14:27:23 -0800

    Added music and lemayo transitions when dying and added lava question mark

---
+++ ExistenceIsPain/Assets/Scripts/WaterController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WaterController : MonoBehaviour {
+
+	//how far away and how fast we rotate
+
+
+	//point and angle at which we move around
+
+
+	// Use this for initialization
+	public virtual void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	public virtual void Update () {
+		
+	}
+
+}
