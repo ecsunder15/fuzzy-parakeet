486473f8a6e2cc3c1976f6e880f6720527a8ff8a
ecsunder15 <esunderland@renewfinancial.com>
2017-11-19 14:24:42 -0800

    new scene with enemy that fires health packs at you

--- ExistenceIsPain/Assets/Scripts/EnemyController.cs
+++ ExistenceIsPain/Assets/Scripts/EnemyController.cs
@@ -8,22 +8,52 @@ public class EnemyController : MonoBehaviour {
 	public float rotateSpeed;
 	public float radius;
 
+	//health pack bullets
+	public GameObject healthPrefab;
+	public Transform healthSpawn;
+
 	//point and angle at which we move around
 	private Vector2 target;
 	private float angle;
 
+	//shot timer vars
+	private float shotTimer;
+	public float timeOfLastShot;
+	public float waitingTime;
+
 	// Use this for initialization
-	public virtual void Start () {
+	public void Start () {
 		target = transform.position;
 	}
 	
 	// Update is called once per frame
-	public virtual void Update () {
+	public void Update () {
 		angle += rotateSpeed * Time.deltaTime;
+		shotTimer = Time.fixedTime;
 
 		//set the circular angle point thing to rotate around
 		var offset = new Vector2(Mathf.Sin(angle), Mathf.Cos(angle)) * radius;
 		transform.position = target + offset;
+
+		//fire a health pack every 2 seconds
+		if (shotTimer - timeOfLastShot > 2) {
+			timeOfLastShot = shotTimer;
+			Fire ();
+		}
+	}
+
+	void Fire() {
+		// Create the Bullet from the Bullet Prefab
+		var healthPack = (GameObject)Instantiate (
+			healthPrefab,
+			healthSpawn.position,
+			healthSpawn.rotation);
+
+		// Add velocity to the bullet
+		//healthPack.GetComponent<Rigidbody>().velocity = healthPack.transform.forward * 3;
+
+		// Destroy the bullet after 2 seconds
+		Destroy(healthPack, 2.0f);
 	}
 
 }
